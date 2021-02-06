5f16532f97e9f952cc0a2aca7a2ab19463db969e
jovankaruna <jovancahyadi@gmail.com>
2021-02-06 14:03:20 +0700

    setup html css js

---
+++ public/main.js
@@ -0,0 +1,17 @@
+var canvas;
+var gl;
+
+const init = () => {
+  canvas = document.getElementById("main-canvas");
+
+  gl = canvas.getContext("webgl");
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+  //  Configure WebGL
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+};
+
+window.onload = init;
