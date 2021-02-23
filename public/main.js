04479a195d7d95fc9919e5912e3762dca660b18e
jovankaruna <jovancahyadi@gmail.com>
2021-02-23 09:25:10 +0700

    Fix canvas offset, new line method

--- public/main.js
+++ public/main.js
@@ -155,25 +155,18 @@ const eventListener = () => {
   render();
 };
 
-const createLine = (begin, end) => {
-  var width = 0.005;
-  var beta = Math.PI / 2.0 - Math.atan2(end[1] - begin[1], end[0] - begin[0]);
-  var delta_x = Math.cos(beta) * width;
-  var delta_y = Math.sin(beta) * width;
-  return [
-    vec2(begin[0] - delta_x, begin[1] + delta_y),
-    vec2(begin[0] + delta_x, begin[1] - delta_y),
-    vec2(end[0] + delta_x, end[1] - delta_y),
-    vec2(end[0] - delta_x, end[1] + delta_y),
-  ];
+const createLine = (start, end) => {
+  return [start[0], start[1], end[0], end[1]];
 };
 
 const render = () => {
+  // var renderedLine = [];
+  
   gl.clear(gl.COLOR_BUFFER_BIT);
   if (currPts.length == 2) {
     var tempPts = createLine(currPts[0], currPts[1]);
     points.push(tempPts[0], tempPts[1], tempPts[2], tempPts[3]);
-    for (var i = 0; i < 4; ++i) {
+    for (var i = 0; i < 2; ++i) {
       colors.push(lineColor[0], lineColor[1], lineColor[2]);
     }
     currPts.shift();
@@ -183,9 +176,8 @@ const render = () => {
   gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
   for (var i = 0; i < points.length / 4; i++) {
-    gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+    gl.drawArrays(gl.LINES, 2 * i, 2);
   }
-  console.log(points);
 };
 
 window.onload = init;
